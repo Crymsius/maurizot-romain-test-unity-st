f897db6ebbaf1fe812ba6135290219e8aee5950e
Crymsius <romain.maurizot@gmail.com>
2019-02-27 12:53:26 +0100

    Adding editor, first movement scripts for pieces, prefabs for each piece and very first grid

---
+++ Assets/Scripts/Editor/PieceEditor.cs
@@ -0,0 +1,94 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceEditor : EditorWindow {
+
+	public Object tile;
+	string pieceName;
+
+	int height = 5;
+	int width = 5;
+	int previousHeight;
+	int previousWidth;
+
+	string newGridButton = "New Grid";
+	string createButton = "Create";
+
+	public bool [][] cells;
+
+
+	[MenuItem ("Custom/Piece Editor")]
+
+	public static void ShowWindow () {
+		EditorWindow.GetWindow(typeof(PieceEditor));
+		
+	}
+	void OnGUI () {
+		
+		height = EditorGUILayout.IntField("height of the grid:", height);
+		width = EditorGUILayout.IntField("width of the grid:", width);
+
+		if (GUILayout.Button(newGridButton))
+		{
+			InitializeGrid();
+		}
+
+		if (height == previousHeight && width == previousWidth) {
+			ShowGrid(previousHeight, previousWidth);
+		}
+
+
+		pieceName = EditorGUILayout.TextField("Name of the Piece :", pieceName);
+
+		tile = EditorGUILayout.ObjectField("Tile Object", tile, typeof(Object), false);
+
+		if (GUILayout.Button(createButton))
+		{
+			CreatePiece();
+		}
+	}
+
+	void InitializeGrid() {
+		cells= new bool[height][];
+		for (int i = 0; i < height; i++)
+		{
+			cells[i] = new bool[width];
+		}
+		previousHeight = height;
+		previousWidth = width;
+	}
+
+	void ShowGrid(int h, int w) {
+
+		for (int i = 0; i < h; i++)
+		{
+			EditorGUILayout.BeginHorizontal();
+			for (int j = 0; j < w; j++)
+			{
+				cells[i][j] =EditorGUILayout.Toggle(cells[i][j]);
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+
+
+	void CreatePiece() {
+		Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/Temporary/"+pieceName+".prefab");
+
+		GameObject piece = new GameObject("piece");
+		for (int i = 0; i < previousHeight; i++)
+		{
+			for (int j = 0; j < previousWidth; j++)
+			{
+				if (cells[i][j]) {
+					GameObject.Instantiate(tile, new Vector3(j, -i), Quaternion.identity, piece.transform);
+				}
+			}
+		}
+
+		PrefabUtility.ReplacePrefab(piece.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+	}
+
+}
---
+++ Assets/Scripts/PieceScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceScript : MonoBehaviour {
+
+	float verticalSpeed;
+	float horizontalSpeed;
+	float timeRemainingVertical;
+
+
+	// Use this for initialization
+	void Start () {
+		verticalSpeed = 1f;
+		horizontalSpeed = 1f;
+		timeRemainingVertical = verticalSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		timeRemainingVertical -= Time.deltaTime;
+		if ( timeRemainingVertical < 0 )
+		{
+			GoDown();
+			timeRemainingVertical = verticalSpeed;
+		}
+
+
+		if (Input.GetButtonDown("Horizontal")) {
+			float direction = Mathf.Sign(Input.GetAxis("Horizontal"));
+			float distance = Mathf.Floor(horizontalSpeed) * direction;
+			gameObject.transform.Translate(distance,0,0);
+		}
+
+		if (Input.GetButtonDown("Rotation")) {
+			float direction = Mathf.Sign(Input.GetAxis("Rotation"));
+			float rotation = 90f * direction;
+			gameObject.transform.GetChild(0).Rotate(new Vector3(0,0,rotation));
+		}
+	}
+
+	void GoDown() {
+		//if possible
+		gameObject.transform.Translate(Vector3.down);
+	}
+
+
+
+}
